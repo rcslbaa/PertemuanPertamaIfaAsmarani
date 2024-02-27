ce1a923eacf0008bb374e0196ef51e1da2e455b5
rcslbaa <ifasmarani3125@gmail.com>
2024-02-27 18:51:53 +0700

    Add project files.

---
+++ PertemuanPertama/PertemuanPertama.cpp
@@ -0,0 +1,18 @@
+// PertemuanPertama.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{//begin
+    float nJejari, nLuas;
+    // numeric nJejari, nLuas
+    cout << "masukan jejari = ";
+    // display "Masukan jejari"
+    cin >> nJejari;
+    // accept nJejari
+    nLuas = 3.14 * nJejari * nJejari;
+    cout << "luasnya = " << nLuas << endl;
+}
+
